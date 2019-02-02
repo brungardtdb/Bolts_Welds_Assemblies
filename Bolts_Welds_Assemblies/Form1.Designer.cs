0a0e1b2b398c53aec6f6f1fca17d3194ca22a4a7
David Brungardt <brungardtdb@gmail.com>
2019-02-02 10:44:43 -0700

    Initial commit

---
+++ Bolts_Welds_Assemblies/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Bolts_Welds_Assemblies
+{
+    partial class frm_Bolts_Welds_Assemblies
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Bolt = new System.Windows.Forms.Button();
+            this.btn_Weld = new System.Windows.Forms.Button();
+            this.btn_Add_To = new System.Windows.Forms.Button();
+            this.btn_Get_Assembly = new System.Windows.Forms.Button();
+            this.btn_Set_Workplane = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Bolt
+            // 
+            this.btn_Bolt.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_Bolt.Location = new System.Drawing.Point(38, 12);
+            this.btn_Bolt.Name = "btn_Bolt";
+            this.btn_Bolt.Size = new System.Drawing.Size(90, 23);
+            this.btn_Bolt.TabIndex = 0;
+            this.btn_Bolt.Text = "Bolt";
+            this.btn_Bolt.UseVisualStyleBackColor = false;
+            this.btn_Bolt.Click += new System.EventHandler(this.btn_Bolt_Click);
+            // 
+            // btn_Weld
+            // 
+            this.btn_Weld.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_Weld.Location = new System.Drawing.Point(38, 42);
+            this.btn_Weld.Name = "btn_Weld";
+            this.btn_Weld.Size = new System.Drawing.Size(90, 23);
+            this.btn_Weld.TabIndex = 1;
+            this.btn_Weld.Text = "Weld";
+            this.btn_Weld.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btn_Weld.UseVisualStyleBackColor = false;
+            this.btn_Weld.Click += new System.EventHandler(this.btn_Weld_Click);
+            // 
+            // btn_Add_To
+            // 
+            this.btn_Add_To.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_Add_To.Location = new System.Drawing.Point(38, 71);
+            this.btn_Add_To.Name = "btn_Add_To";
+            this.btn_Add_To.Size = new System.Drawing.Size(90, 23);
+            this.btn_Add_To.TabIndex = 2;
+            this.btn_Add_To.Text = "Add To";
+            this.btn_Add_To.UseVisualStyleBackColor = false;
+            this.btn_Add_To.Click += new System.EventHandler(this.btn_Add_To_Click);
+            // 
+            // btn_Get_Assembly
+            // 
+            this.btn_Get_Assembly.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_Get_Assembly.Location = new System.Drawing.Point(38, 100);
+            this.btn_Get_Assembly.Name = "btn_Get_Assembly";
+            this.btn_Get_Assembly.Size = new System.Drawing.Size(90, 23);
+            this.btn_Get_Assembly.TabIndex = 3;
+            this.btn_Get_Assembly.Text = "Get Assembly";
+            this.btn_Get_Assembly.UseVisualStyleBackColor = false;
+            this.btn_Get_Assembly.Click += new System.EventHandler(this.btn_Get_Assembly_Click);
+            // 
+            // btn_Set_Workplane
+            // 
+            this.btn_Set_Workplane.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_Set_Workplane.Location = new System.Drawing.Point(158, 12);
+            this.btn_Set_Workplane.Name = "btn_Set_Workplane";
+            this.btn_Set_Workplane.Size = new System.Drawing.Size(91, 23);
+            this.btn_Set_Workplane.TabIndex = 4;
+            this.btn_Set_Workplane.Text = "Set Workplane";
+            this.btn_Set_Workplane.UseVisualStyleBackColor = false;
+            this.btn_Set_Workplane.Click += new System.EventHandler(this.btn_Set_Workplane_Click);
+            // 
+            // frm_Bolts_Welds_Assemblies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(295, 142);
+            this.Controls.Add(this.btn_Set_Workplane);
+            this.Controls.Add(this.btn_Get_Assembly);
+            this.Controls.Add(this.btn_Add_To);
+            this.Controls.Add(this.btn_Weld);
+            this.Controls.Add(this.btn_Bolt);
+            this.Name = "frm_Bolts_Welds_Assemblies";
+            this.Text = "Bolts Welds and Assemblies";
+            this.Load += new System.EventHandler(this.frm_Bolts_Welds_Assemblies_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Bolt;
+        private System.Windows.Forms.Button btn_Weld;
+        private System.Windows.Forms.Button btn_Add_To;
+        private System.Windows.Forms.Button btn_Get_Assembly;
+        private System.Windows.Forms.Button btn_Set_Workplane;
+    }
+}
+
---
+++ Bolts_Welds_Assemblies/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Tekla Structures Namespaces
+using Tekla.Structures.Model;
+using T3D = Tekla.Structures.Geometry3d;
+using TSMUI = Tekla.Structures.Model.UI;
+using System.Collections;
+
+namespace Bolts_Welds_Assemblies
+{
+    public partial class frm_Bolts_Welds_Assemblies : Form
+    {
+        Model currentModel;
+
+        public frm_Bolts_Welds_Assemblies()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Bolts_Welds_Assemblies_Load(object sender, EventArgs e)
+        {
+            // try connecting to model
+            try
+            {
+                currentModel = new Model();
+            }
+            catch
+            {
+                MessageBox.Show("Model may not be connected.");
+            }
+        }
+
+        private void btn_Bolt_Click(object sender, EventArgs e)
+        {
+            // Current Workplane. Remember how the user had the model before you made changes
+            TransformationPlane originalPlane = currentModel.GetWorkPlaneHandler().GetCurrentTransformationPlane();
+
+            TSMUI.Picker thisPicker = new TSMUI.Picker();
+            Part firstPickedPart = null;
+            Part secondPickedPart = null;
+
+            try
+            {
+                firstPickedPart = thisPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+                secondPickedPart = thisPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+            }
+            catch
+            {
+                firstPickedPart = null;
+                secondPickedPart = null;
+            }
+
+            if (firstPickedPart != null && secondPickedPart != null)
+            {
+                // Change the workplane to the coordinate system of hte part
+                currentModel.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane(firstPickedPart.GetCoordinateSystem()));
+
+                // BOltGroupCode
+                BoltArray newBoltArray = new BoltArray();
+                newBoltArray.BoltSize = 25.4;
+                newBoltArray.BoltType = BoltGroup.BoltTypeEnum.BOLT_TYPE_WORKSHOP;
+                newBoltArray.BoltStandard = "A325N";
+                newBoltArray.CutLength = 150;
+                // Adds to spacings of bolts in X direction
+                newBoltArray.AddBoltDistX(76.2);
+                newBoltArray.AddBoltDistX(76.2);
+                // Only one row of bolts
+                newBoltArray.AddBoltDistY(0);
+                // Edge distance from first pooint picked to first bolt in x direction
+                newBoltArray.StartPointOffset.Dx = 38.1;
+                // Front lines up nicely with x / y position in current workplane.
+                newBoltArray.Position.Rotation = Position.RotationEnum.FRONT;
+                newBoltArray.PartToBoltTo = firstPickedPart;
+                newBoltArray.PartToBeBolted = secondPickedPart;
+                newBoltArray.FirstPosition = new T3D.Point(0, 100, 0);
+                newBoltArray.SecondPosition = new T3D.Point(1000, 250, 0);
+
+                if (newBoltArray.Insert())
+                {
+                    // Draw X Axis of bolt group
+                    TSMUI.GraphicsDrawer thisDrawer = new TSMUI.GraphicsDrawer();
+                    thisDrawer.DrawLineSegment(new T3D.LineSegment(newBoltArray.FirstPosition, newBoltArray.SecondPosition), new TSMUI.Color(1, 0, 0));
+
+                    // Set workplane back to what user had before
+                    currentModel.GetWorkPlaneHandler().SetCurrentTransformationPlane(originalPlane);
+
+                    // Show the bolt group in the model but the user will never see the workplane change.
+                    currentModel.CommitChanges();
+                }
+                else
+                {
+
+                }
+            }            
+
+        }
+
+        private void btn_Weld_Click(object sender, EventArgs e)
+        {
+            TSMUI.Picker thisPicker = new TSMUI.Picker();
+            Part firstPickedPart = null;
+            Part secondPickedPart = null;
+
+            try
+            {
+                firstPickedPart = thisPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+                secondPickedPart = thisPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+            }
+            catch 
+            {
+                firstPickedPart = null;
+                secondPickedPart = null;
+            }
+
+            if(firstPickedPart != null && secondPickedPart != null)
+            {
+                Weld newWeld = new Weld();
+                newWeld.MainObject = firstPickedPart;
+                newWeld.SecondaryObject = secondPickedPart;
+                newWeld.SizeBelow = 6.35;
+                newWeld.TypeBelow = BaseWeld.WeldTypeEnum.WELD_TYPE_FILLET;
+                newWeld.SizeAbove = 0.0;
+                newWeld.TypeAbove = BaseWeld.WeldTypeEnum.WELD_TYPE_NONE;
+                newWeld.AroundWeld = false;
+                newWeld.ShopWeld = true;
+                newWeld.ReferenceText = "Typ.";
+                newWeld.Placement = BaseWeld.WeldPlacementTypeEnum.PLACEMENT_AUTO;
+
+                if (newWeld.Insert())
+                {
+                    // Show the weld in the model but the user will never see the workplane chage
+                    currentModel.CommitChanges();
+                }
+                else
+                {
+                    Tekla.Structures.Model.Operations.Operation.DisplayPrompt("Weld  was not created");
+                }
+            }
+        }
+
+        private void btn_Add_To_Click(object sender, EventArgs e)
+        {
+            TSMUI.Picker thisPicker = new TSMUI.Picker();
+            Part firstPickedPart = null;
+            Part secondPickedPart = null;
+
+            try
+            {
+                firstPickedPart = thisPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+                secondPickedPart = thisPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+            }
+            catch
+            {
+                firstPickedPart = null;
+                secondPickedPart = null;
+            }
+
+            if (firstPickedPart != null && secondPickedPart != null)
+            {
+                Assembly mainAssembly = firstPickedPart.GetAssembly();
+                mainAssembly.Add(secondPickedPart);
+                mainAssembly.Modify();
+                currentModel.CommitChanges();
+            }
+            else
+            {
+                Tekla.Structures.Model.Operations.Operation.DisplayPrompt("Could not add parts to assembly");
+            }
+        }
+
+        private void btn_Get_Assembly_Click(object sender, EventArgs e)
+        {
+            TSMUI.Picker thisPicker = new TSMUI.Picker();
+            Part pickedPart = null;
+
+            try
+            {
+                pickedPart = thisPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+            }
+            catch
+            {
+                pickedPart = null;
+            }
+
+            if (pickedPart!= null)
+            {
+                Assembly thisAssembly = pickedPart.GetAssembly();
+                ModelObject mainObject = thisAssembly.GetMainPart();
+                Part mainPart = mainObject as Part;
+                
+                if (mainPart != null)
+                {
+                    mainPart.Class = "2";
+                    mainPart.Modify();
+                    // Don't grab from assembly because it doesn't have one...
+                    mainPart.GetCoordinateSystem();
+
+                    // Set the mainPart of the assembly.
+                    // thisAssembly.setMainPart(pickedPart);
+
+                }
+                else
+                {
+                    Assembly mainAssembly = mainObject as Assembly;
+                    if (mainAssembly != null)
+                    {
+                        // You got an assembly...
+                    }
+                }
+
+                // This doesn't do anything, don't use it
+                ModelObjectEnumerator partsOnAssembly = thisAssembly.GetChildren();
+                int childrenObjects = partsOnAssembly.GetSize();
+
+                // Gets Parts
+                ArrayList partsList = thisAssembly.GetSecondaries();
+
+                foreach(Part thisPart in partsList)
+                {
+                    thisPart.Class = "6";
+                    thisPart.Modify();
+                }
+
+                // Neat way to highlight / select objects in the model.
+                TSMUI.ModelObjectSelector selector = new TSMUI.ModelObjectSelector();
+                selector.Select(partsList);
+
+                // Gets Sub Assemblies
+                int subAssemblies = thisAssembly.GetSubAssemblies().Count;
+                Tekla.Structures.Model.Operations.Operation.DisplayPrompt(childrenObjects + " Children Objects");
+
+                currentModel.CommitChanges();
+            }
+        }
+
+        private void btn_Set_Workplane_Click(object sender, EventArgs e)
+        {
+            // Reset workplane back to global
+            currentModel.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane());
+
+            TSMUI.Picker currentPicker = new TSMUI.Picker();
+            Part pickedPart = null;
+
+            try
+            {
+                pickedPart = currentPicker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+            }
+            catch
+            {
+
+                pickedPart = null;
+            }
+
+            if (pickedPart != null)
+            {
+                // Change the workplane to the coordinate system of the plate
+                currentModel.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane(pickedPart.GetCoordinateSystem()));
+
+                // Show the plate in the model and the workplane change
+                currentModel.CommitChanges();
+
+            }
+        }
+    }
+}
